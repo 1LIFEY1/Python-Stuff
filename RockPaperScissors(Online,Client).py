2b5fac5ccff37a8f6d99055b074bc4ce1654eefe
1LIFEY1 <113545618+1LIFEY1@users.noreply.github.com>
2022-09-29 16:56:04 +0200

    Update RockPaperScissors(Online,Client).py

--- RockPaperScissors(Online,Client).py
+++ RockPaperScissors(Online,Client).py
@@ -5,7 +5,7 @@ import json
 
 # Connnection To The Host
 Client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-Client.connect(("192.168.1.101",80))
+Client.connect(("192.168.1.101",8888))
 
 # Send To The Server Info For Creating A Client
 Nick = input("Enter Ur NickName:")
@@ -14,14 +14,14 @@ Pass = input("Enter The Game Id:")
 Client.send(Nick.encode('utf-8'))
 Client.send(Pass.encode('utf-8'))
 # Waiting For A Response
-packets = (Client.recv(1023).decode('utf-8'),Client.recv(1023).decode('utf-8'))
-print(packets[1],"\n",packets[2])
+packet,packet1 = Client.recv(1023).decode('utf-8'),Client.recv(1023).decode('utf-8')
+print(packet,"\n",packet1)
 
 # Game
 while True:
    Client.send(input("Enter 'scissors' or 'rock' or 'paper':").encode('utf-8'))
-   packets2 = (Client.recv(1023).decode('utf-8'),Client.recv(1023).decode('utf-8'),Client.recv(1023).decode('utf-8'))
-   print(packets2[1],"\n",packets2[2],"\n",packets2[3])
+   move,winner,points = Client.recv(1023).decode('utf-8'),Client.recv(1023).decode('utf-8'),Client.recv(1023).decode('utf-8')
+   print(move,"\n",winner,"\n",points)
    packet = json.loads(Client.recv(1024).decode('utf-8'))
    if packet["1"] == 3 or packet["2"] == 3:
       Client.close()
